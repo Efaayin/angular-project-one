1eda4ddb08e2b7bec35be0c642586f6d94daa973
Efaayin <alichiifeanyi@yahoo.com>
2021-02-12 22:38:46 +0100

    further updates

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'this is simply a test', 'https://thumbor.thedailymeal.com/dXE81KIfC03dEPM40wn00fxi7Gc=/870x565/filters:format(webp)/https://www.thedailymeal.com/sites/default/files/slideshows/2046132/2227952/1.jpg')
+  ];
 
   constructor() { }
 
